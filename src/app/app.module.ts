d973fab60f40fce66cacc23df5fa4215aa6d986a
cpchen <305983806@qq.com>
2019-11-27 10:51:57 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
+import { LoginComponent } from './pages/login/login.component';
 
 registerLocaleData(zh);
 
+/**
+ * 一个 Module 指的是使用 @NgModule 修饰的 class。
+ */
+
 @NgModule({
+  // 模块内部Components/Directives/Pipes的列表，声明一下这个模块内部成员
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
+  // 导入其他module，其它module暴露的出的Components、Directives、Pipes等可以在本module的组件中被使用。
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,7 +32,9 @@ registerLocaleData(zh);
     HttpClientModule,
     BrowserAnimationsModule
   ],
+  // 指定应用程序的根级别需要使用的service。
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  // 声明模块的主组件是什么
   bootstrap: [AppComponent]
 })
 export class AppModule { }
